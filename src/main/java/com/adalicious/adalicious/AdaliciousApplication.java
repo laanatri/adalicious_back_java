7d3420429c6032f0b6b7cca528c13ae91f8d2d6b
laanatri <laanatri.ade@gmail.com>
2025-06-16 16:03:28 +0200

    init, first commit, add user

---
+++ src/main/java/com/adalicious/adalicious/AdaliciousApplication.java
@@ -0,0 +1,13 @@
+package com.adalicious.adalicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaliciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaliciousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adalicious/adalicious/AdaliciousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adalicious.adalicious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaliciousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
